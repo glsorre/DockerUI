6c510c07d11f71c6615e8bacefb4f485f9d8e0ea
Giuseppe Sorrentino <uhpnou@gmail.com>
2020-04-24 23:21:47 +0200

    beginned images

--- src/dockerui/app.py
+++ src/dockerui/app.py
@@ -9,12 +9,12 @@ import wx
 import wx.lib.inspection
 import wx.xrc
 
-from src.dockerui.utils import Container
+from src.dockerui.utils import Container, Image
 
 RSRC_FILE = "src/dockerui/app.xrc"
 RSRC_FRAMENAME = "mainFrame"
 
-BORDER_MAIN = 5
+BORDER_MAIN = 1
 
 client = docker.from_env()
 
@@ -35,15 +35,24 @@ class DockerUI(wx.App):
         self.frame = self.res.LoadFrame(None, RSRC_FRAMENAME)
         self.SetTopWindow(self.frame)
         self.notebook = wx.xrc.XRCCTRL(self.frame, "mainNotebook")
-        self.window = wx.xrc.XRCCTRL(self.frame, "containersWindow")
-        self.panel = wx.xrc.XRCCTRL(self.frame, "containersPanel")
-        self.page = wx.xrc.XRCCTRL(self.frame, "containersPage")
-        self.window.SetScrollRate(BORDER_MAIN,BORDER_MAIN)
+
+        self.containers_window = wx.xrc.XRCCTRL(self.frame, "containersWindow")
+        self.containers_panel = wx.xrc.XRCCTRL(self.frame, "containersPanel")
+        self.containers_page = wx.xrc.XRCCTRL(self.frame, "containersPage")
+        self.containers_window.SetScrollRate(BORDER_MAIN, BORDER_MAIN)
+
+        self.images_window = wx.xrc.XRCCTRL(self.frame, "imagesWindow")
+        self.images_panel = wx.xrc.XRCCTRL(self.frame, "imagesPanel")
+        self.images_page = wx.xrc.XRCCTRL(self.frame, "imagesPage")
+        self.images_window.SetScrollRate(BORDER_MAIN, BORDER_MAIN)
+
         self.frame.SetSize(600,600)
         self.frame.Bind(wx.EVT_SIZE, self.on_size)
+
         self.frame.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_APPWORKSPACE))
         self.notebook.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_APPWORKSPACE))
-        self.panel.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_WINDOW))
+        self.containers_panel.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_WINDOW))
+        self.images_panel.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_WINDOW))
 
     def menu(self):
         menu_bar  = wx.MenuBar()
@@ -81,34 +90,70 @@ class DockerUI(wx.App):
         containers_list = self.get_containers_list()
         print(containers_list)
 
-        self.panel.Hide()
+        self.containers_panel.Hide()
         if state["is_list"]:
             new_sizer = wx.BoxSizer(wx.VERTICAL)
         else:
             new_sizer = wx.WrapSizer()
 
-        if self.panel.GetSizer().GetItemCount() > 0:
-            self.panel.DestroyChildren()
-            self.panel.GetSizer().Remove(0)
+        if self.containers_panel.GetSizer().GetItemCount() > 0:
+            self.containers_panel.DestroyChildren()
+            self.containers_panel.GetSizer().Remove(0)
 
         for c in containers_list:
-            static = Container(self.panel, self, state, c)
+            static = Container(self.containers_panel, self, state, c)
             state['containers'].append(c)
             new_sizer.Add(static, 0, wx.ALL|wx.EXPAND, border=BORDER_MAIN)
         
-        self.panel.GetSizer().Add(new_sizer, 0, wx.ALL|wx.EXPAND)
-        self.panel.Show()
+        self.containers_panel.GetSizer().Add(new_sizer, 0, wx.ALL|wx.EXPAND)
+        self.containers_panel.Show()
+        self.refresh_view()
+
+    def refresh_images(self, scheduler, state):
+        print("refreshing images")
+        images_list = self.get_images_list()
+        print(images_list)
+
+        self.images_panel.Hide()
+        if state["is_list"]:
+            new_sizer = wx.BoxSizer(wx.VERTICAL)
+        else:
+            new_sizer = wx.WrapSizer()
+
+        if self.images_panel.GetSizer().GetItemCount() > 0:
+            self.images_panel.DestroyChildren()
+            self.images_panel.GetSizer().Remove(0)
+
+        for c in images_list:
+            static = Image(self.images_panel, self, state, c)
+            state['images'].append(c)
+            new_sizer.Add(static, 0, wx.ALL|wx.EXPAND, border=BORDER_MAIN)
+        
+        self.images_panel.GetSizer().Add(new_sizer, 0, wx.ALL|wx.EXPAND)
+        self.images_panel.Show()
         self.refresh_view()
 
     def refresh_view(self):
-        print("resizing")
-        page_size = self.page.GetMinSize()
-        sizer_size = self.panel.GetSizer().CalcMin()
-        container_bigger_size = self.panel.GetChildren()[self.get_bigger_container()].GetSize()
-        self.panel.SetMinSize((container_bigger_size[0], sizer_size[1]))
-        self.panel.SetMaxSize((page_size[0], (sys.maxsize * 2 + 1)))
-        self.panel.GetSizer().Layout()
-        self.panel.Layout()
+        print("resizing containers panel")
+        containers_page_size = self.containers_page.GetMinSize()
+        containers_sizer_size = self.containers_panel.GetSizer().CalcMin()
+        container_bigger_size = self.containers_panel.GetChildren()[self.get_bigger_container()].GetSize()
+        self.containers_panel.SetMinSize((container_bigger_size[0], containers_sizer_size[1]))
+        self.containers_panel.SetMaxSize((containers_page_size[0], (sys.maxsize * 2 + 1)))
+        self.containers_panel.GetSizer().Layout()
+        self.containers_panel.Layout()
+
+        print("resizing images panel")
+        images_page_size = self.images_page.GetMinSize()
+        images_sizer_size = self.images_panel.GetSizer().CalcMin()
+        if self.images_panel.GetChildren():
+            image_bigger_size = self.images_panel.GetChildren()[self.get_bigger_image()].GetSize()
+        else:
+            image_bigger_size = (0, 0)
+        self.images_panel.SetMinSize((image_bigger_size[0], images_sizer_size[1]))
+        self.images_panel.SetMaxSize((images_page_size[0], (sys.maxsize * 2 + 1)))
+        self.images_panel.GetSizer().Layout()
+        self.images_panel.Layout()
 
     def on_size(self, event):
         self.refresh_view()
@@ -118,6 +163,14 @@ class DockerUI(wx.App):
         print("getting containers list")
         text = self.text_ctrl.GetValue()
         result = client.containers.list(True, filters={"name":self.text_ctrl.GetValue()})
+        result = sorted(result, key=lambda c: c.name)
+        return result
+
+    def get_images_list(self):
+        print("getting images list")
+        text = self.text_ctrl.GetValue()
+        result = client.images.list(all=True)
+        #result = sorted(result, key=lambda i: i.name)
         return result
 
     def refresh_action(self, event):
@@ -134,10 +187,22 @@ class DockerUI(wx.App):
         bigger = 0
         bigger_size = 0
 
-        for i, c in enumerate(self.panel.GetChildren()):
+        for i, c in enumerate(self.containers_panel.GetChildren()):
             if c.GetSize()[0] > bigger_size:
                 bigger = i
                 bigger_size = c.GetSize()[0] + BORDER_MAIN
+
+        print(f"the bigger container is n {bigger}")
+        return bigger
+
+    def get_bigger_image(self):
+        bigger = 0
+        bigger_size = 0
+
+        for i, img in enumerate(self.images_panel.GetChildren()):
+            if img.GetSize()[0] > bigger_size:
+                bigger = i
+                bigger_size = img.GetSize()[0] + BORDER_MAIN
 
         print(f"the bigger container is n {bigger}")
         return bigger
@@ -155,6 +220,7 @@ def main():
 
     app.frame.Bind(wx.EVT_CLOSE, lambda e: (app.scheduler.cancel_all(), e.Skip()))
     app.scheduler.schedule(app.refresh_containers, state)
+    app.scheduler.schedule(app.refresh_images, state)
 
     wx.lib.inspection.InspectionTool().Show()
 
